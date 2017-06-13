4abb5dc9c6a9cca01f20c8d4a48163ff288bba07
Hiroki Sawano <hiroki.sawano.2512@gmail.com>
2017-06-14 01:54:10 +0900

    automated glassfish admin settings

--- cookbooks/sample/recipes/default.rb
+++ cookbooks/sample/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package %w(java-1.8.0-openjdk unzip wget vim git) do
+package %w(java-1.8.0-openjdk unzip wget vim git expect) do
   action :install
 end
 
@@ -66,13 +66,29 @@ service 'glassfish' do
   supports :start => true, :stop => true, :reload => true
 end
 
-# allow accessing admin console 
-# asadmin change-admin-password
-# asadmin enable-secure-admin
-# asadmin stop-domain
-# asadmin start-domain
+cookbook_file "#{glassfish_home}/glassfish_setting.exp" do
+  source 'glassfish_setting.exp'
+  owner 'glassfish'
+  group 'glassfish'
+  mode '0644'
+  action :create_if_missing
+end
 
-# doesn't work?
+# have to check if this setting has already been done
+# should be able to give parameters in json file
+# going to change 'touch done' part
+bash 'setup glassfish' do
+  user 'glassfish'
+  group 'glassfish'
+  cwd "#{glassfish_home}"
+  code <<-EOC
+    expect glassfish_setting.exp "admin" "" "password"
+    touch done
+  EOC
+  not_if { File.exists?("#{glassfish_home}/done")}
+end
+
+# doesn't work?  it shows up to date when executed
 git '/home/hiroki.sawano' do
    repository 'https://github.com/hiroki-sawano/vimrc.git'
    revision 'master'
