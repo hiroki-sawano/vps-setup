df8c2fa36969b0fa5f0d2d87a0091cfcd84bc5bf
Hiroki Sawano <hiroki.sawano.2512@gmail.com>
2017-06-24 01:18:49 +0900

    modified sample recipe

--- cookbooks/sample/recipes/default.rb
+++ cookbooks/sample/recipes/default.rb
@@ -11,7 +11,7 @@ package %w(java-1.8.0-openjdk unzip wget vim git expect) do
 end
 
 user 'root' do
-  password '$1$rhRWbZkC$00ejJhdnFT9xcZVP797Vh0'
+#  password ''
   action :modify
 end
 
@@ -20,7 +20,7 @@ user 'hiroki.sawano' do
   group 'wheel'
   home '/home/hiroki.sawano'
   shell '/bin/bash'
-  password '$1$s0rYLPDS$5m/LpOEzpljFLEiB5TMwQ'
+#  password ''
   action :create
 end
  
@@ -36,10 +36,11 @@ user 'glassfish' do
   group 'glassfish'
   home "#{glassfish_home}" 
   shell '/bin/bash'
-  password '$1$Uc75MVvV$6eXsMI41mi1g6xZI0HEum'
+#  password ''
   action :create
 end
 
+# should not be executed twice
 remote_file "#{glassfish_home}/glassfish-4.1.zip" do
   source 'http://download.java.net/glassfish/4.1/release/glassfish-4.1.zip'
   owner 'glassfish'
@@ -79,6 +80,7 @@ cookbook_file "#{glassfish_home}/glassfish_setting.exp" do
   action :create_if_missing
 end
 
+# i think it can call service action, not using systemctl directly
 bash 'setup glassfish' do
   user 'root'
   group 'root'
@@ -91,6 +93,7 @@ bash 'setup glassfish' do
   EOC
   not_if { File.exists?("#{glassfish_home}/done")}
 end
+
 # have to check if this setting has already been done
 # should be able to give parameters in json file
 # going to change 'touch done' part
